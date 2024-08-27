051e22d1a808c2bb3ccfdf5e9b040f5afcaba014
Woody <73010620+science-direct@users.noreply.github.com>
2024-08-27 23:49:33 +0800

    Update scipt.js

--- redirect/auth/a7965398c2ba73f32feeb0c69331d0347c42837d76408b67582b27abf1dc20517efddf10485039a4642767a717dafd1a/scipt.js
+++ redirect/auth/a7965398c2ba73f32feeb0c69331d0347c42837d76408b67582b27abf1dc20517efddf10485039a4642767a717dafd1a/scipt.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     fetch('0e874a77969412fd2d8ccd205dbe39f3664b5b8e01030fd46f6a920e785c797933bc270bb6dd4be6d4d9097d92facbcf.json')
-        .then(response => response.json())
+        .then(response => response.json()) 
         .then(data => {
             if (md5(document.getElementById("password").value) === data.hashedPassword) {
                 document.getElementById("message").textContent = "Password is correct!";
